dde26f4aa2d0eeb7512165d11d83a3223b829f15
Antoine Colson <antoine.colson.ratelle@umontreal.ca>
2021-12-11 01:31:36 -0500

    clean up coords_around thanks to https://github.com/MathProuv

--- 2021/2021_11.py
+++ 2021/2021_11.py
@@ -8,22 +8,12 @@ grid = [list(map(int, row)) for row in inp]
 
 def get_coords_around(grid, i, j):
     coords = []
-    if i > 0:
-        coords.append((i - 1, j))
-        if j > 0:
-            coords.append((i - 1, j - 1))
-        if j < len(grid) - 1:
-            coords.append((i - 1, j + 1))
-    if i < len(grid[0]) - 1:
-        coords.append((i + 1, j))
-        if j > 0:
-            coords.append((i + 1, j - 1))
-        if j < len(grid) - 1:
-            coords.append((i + 1, j + 1))
-    if j < len(grid) - 1:
-        coords.append((i, j + 1))
-    if j > 0:
-        coords.append((i, j - 1))
+    for m in range(i - 1, i + 2):
+        for n in range(j - 1, j + 2):
+            if 0 <= m < len(grid) and\
+                    0 <= n < len(grid[0]) and\
+                    not (i == m and j == n):
+                coords.append((m, n))
     return coords
 
 
@@ -32,31 +22,30 @@ def do_round(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             increase(grid, i, j, flashers)
-    for flasher in flashers:
-        grid[flasher[0]][flasher[1]] = 0
+    for i, j in flashers:
+        grid[i][j] = 0
     return grid, flashers
 
 
 def increase(grid, i, j, flashers):
     grid[i][j] = (grid[i][j] + 1) % 10
-    if grid[i][j] == 0:
-        if (i, j) not in flashers:
-            flashers.add((i, j))
-            coords_around = get_coords_around(grid, i, j)
-            for coords in coords_around:
-                increase(grid, coords[0], coords[1], flashers)
+    if grid[i][j] == 0 and (i, j) not in flashers:
+        flashers.add((i, j))
+        for coords in get_coords_around(grid, i, j):
+            increase(grid, coords[0], coords[1], flashers)
 
 
 
-p1 = delta = 0
+p1 = num_flashes = 0
 p2 = 1
 
-while delta < 100:
+while num_flashes < 100:
     grid, flashers = do_round(grid)
-    delta = len(flashers)
+    num_flashes = len(flashers)
 
     if p2 <= 100:
-        p1 += delta
+        p1 += num_flashes
     p2 += 1
 
+p2 -= 1
 print(p1, p2)
