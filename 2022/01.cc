338c62f5093c6863124cac72d487859a0ed22d8b
Antoine Colson <antoine.colson.ratelle@umontreal.ca>
2022-12-03 09:55:14 -0500

    day 1 solutions

--- 2022/01.cc
+++ 2022/01.cc
@@ -1,12 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 
 
 using std::cout;
 using std::endl;
+using std::string;
+using std::ifstream;
+using std::max;
+using std::vector;
+using std::sort;
 
 
 int main()
 {
-  cout << "hello world" << endl;
+  ifstream input("input_1.txt"); 
+  string cur_line;
+  vector<int> totals;
+  int running_sum = 0;
+
+  while (getline(input, cur_line))
+  {
+    if (cur_line.empty())
+    {
+      totals.push_back(running_sum);
+      running_sum = 0;
+    }
+    else
+      running_sum += stoi(cur_line);
+  }
+
+  sort(totals.rbegin(), totals.rend());
+  cout << totals[0] << endl;
+  int solution = totals[0] + totals[1] + totals[2];
+  cout << solution << endl;
+
   return 0;
 }
