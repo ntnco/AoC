5812c9d037281194df56d1168860951384bfc8a2
Antoine Colson <antoine.colson.ratelle@umontreal.ca>
2022-12-05 21:27:14 -0500

    solve day 4

--- 2022/04.cc
+++ 2022/04.cc
@@ -1,33 +1,36 @@
 #include <iostream>
 #include <regex>
 #include <string>
+#include <fstream>
 
 
 using std::cout;
-using std::cin;
 using std::endl;
 using std::smatch;
 using std::regex;
 using std::string;
 using std::regex_search;
-
-
-void p1() {
-  regex e("(\\d+)-(\\d+),(\\d+)-(\\d+)");
-  smatch m;
-  string s;
-
-  while (getline(cin, s))
-  {
-    regex_search(s, m, e);
-    string a = m[1], b = m[2], c = m[3], d = m[4];
-    cout << a << " " << b << " " << c << " " << d;
-  }
-}
+using std::ifstream;
 
 
 int main()
 {
-  p1();
+  ifstream lines("input_4.txt");
+  regex e("(\\d+)-(\\d+),(\\d+)-(\\d+)");
+  smatch m;
+  string s;
+  int p1 = 0,
+      p2 = 0;
+
+  while (getline(lines, s))
+  {
+    regex_search(s, m, e);
+    int a = stoi(m[1]), b = stoi(m[2]), c = stoi(m[3]), d = stoi(m[4]);
+
+    p1 += ((a >= c && b <= d) || (a <= c && b >=d));
+    p2 += (a >= c && a <= d) || (b >= c && b <= d)
+       || (c >= a && c <= b) || (d >= a && d <= b);
+  }
+  cout << p1 << endl << p2 << endl;
   return 0;
 }
